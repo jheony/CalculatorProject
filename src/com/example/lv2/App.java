fb04dcdaa0953588cc8e693a9929663116c96bf2
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-16 15:03:54 +0900

    Calculator 클래스를 추가하여 연산 기능 분리

--- src/com/example/lv2/App.java
+++ src/com/example/lv2/App.java
@@ -1,11 +1,16 @@
-//  **Lv 1.**
-//  계산기는 2개의 숫자를 받을 수 있고 사칙연산 될 문자를 받을 수 있다.
-//  계산기는 exit을 입력할 때까지 계속해서 값을 받고 연산 결과를 반환한다.
+//  **Lv 2.**
+//  1. 계산된 결과 값들을 기록하는 컬렉션을 만든다.
+//  2. 컬렉션의 가장 먼저 저장된 데이터를 삭제하는 기능을 만든다.
+
+package com.example.lv2;
+import com.example.lv2.calculator.Calculator;
 
 import java.util.Scanner;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
         Scanner sc = new Scanner(System.in);
         int value1, value2;
 
@@ -26,35 +31,27 @@ public class Main {
                 char symbol = sc.next().charAt(0);
 
                 // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산 후 결과값 출력 _키워드 : `if` `switch`
-                int result;
-
-                switch (symbol) {
-                    case '+':
-                        result = value1 + value2;
-                        break;
-                    case '-':
-                        result = value1 - value2;
-                        break;
-                    case '*':
-                        result = value1 * value2;
-                        break;
-                    case '/':
-                        if (value2 == 0) {
-                            throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        } else {
-                            result = value1 / value2;
-                        }
-                        break;
-                    default:
-                        throw new RuntimeException("올바른 연산 기호를 입력해주세요.");
-                }
+                int result = calc.calculate(value1, value2, symbol);
                 System.out.println("결과: " + result);
+
+                // 저장된 연산 결과 확인
+                System.out.println(calc.getResults());
 
                 System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         } while (!sc.next().equals("exit"));
+        // 수정 전 결과
+        System.out.println("수정 전 데이터:\n"+calc.getResults());
+
+        // 연산 결과 수정
+        calc.setResults(0, 999);
+        System.out.println("0번째 인덱스를 999로 수정:\n"+calc.getResults());
+
+        // 가장 먼저 저장된 데이터 삭제
+        calc.removeResult();
+        System.out.println("가장 먼저 저장된 데이터 삭제: \n"+calc.getResults());
     }
 }
 
