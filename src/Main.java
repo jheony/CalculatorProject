d0593f284c04bb2e3f248c7bc6c95cd81a7e99db
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-16 14:34:40 +0900

    예외 처리 추가

--- src/Main.java
+++ src/Main.java
@@ -11,40 +11,51 @@ public class Main {
 
         // "exit" 입력 전까지 무한 계산
         do {
-            // 양의 정수(0 포함)를 입력받기
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            value1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            value2 = sc.nextInt();
+            try {
+                // 양의 정수(0 포함)를 입력받기
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                value1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                value2 = sc.nextInt();
+                if (value1 < 0 || value2 < 0) {
+                    throw new Exception("양의 정수(0포함)를 입력해주세요.");
+                }
 
-            // 사칙연산 기호(➕,➖,✖️,➗)를 입력받기
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char symbol = sc.next().charAt(0);
+                // 사칙연산 기호(➕,➖,✖️,➗)를 입력받기
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char symbol = sc.next().charAt(0);
 
-            // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산 후 결과값 출력 _키워드 : `if` `switch`
-            int result = 0;
+                // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산 후 결과값 출력 _키워드 : `if` `switch`
+                int result = 0;
 
-            switch (symbol) {
-                case '+':
-                    result = value1 + value2;
-                    break;
-                case '-':
-                    result = value1 - value2;
-                    break;
-                case '*':
-                    result = value1 * value2;
-                    break;
-                case '/':
-                    if (value2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    } else {
-                        result = value1 / value2;
-                    }
-                    break;
+                switch (symbol) {
+                    case '+':
+                        result = value1 + value2;
+                        break;
+                    case '-':
+                        result = value1 - value2;
+                        break;
+                    case '*':
+                        result = value1 * value2;
+                        break;
+                    case '/':
+                        if (value2 == 0) {
+                            throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        } else {
+                            result = value1 / value2;
+                        }
+                        break;
+                    default:
+                        throw new RuntimeException("올바른 연산 기호를 입력해주세요.");
+                }
+                System.out.println("결과: " + result);
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-            System.out.println("결과: " + result);
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
         } while (!sc.next().equals("exit"));
     }
 }
+
+// 예외 질문 -> 오류 던질때? throw 쓸 때 예외 코드? Exeption or Illig~Argu~Exception? 어떻게 선택해야 하나요?
