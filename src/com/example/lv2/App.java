681bf0454586e62a697f49b394c162dd3854eb15
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-17 10:44:43 +0900

    코드리뷰 피드백: 네이밍 확실하고 신중하게, List 구현체 사용 지양

--- src/com/example/lv2/App.java
+++ src/com/example/lv2/App.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+        Calculator calculator = new Calculator();
 
         Scanner sc = new Scanner(System.in);
         int value1, value2;
@@ -28,14 +28,14 @@ public class App {
 
                 // 사칙연산 기호(➕,➖,✖️,➗)를 입력받기
                 System.out.print("사칙연산 기호를 입력하세요: ");
-                char symbol = sc.next().charAt(0);
+                char operator = sc.next().charAt(0);
 
                 // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산 후 결과값 출력 _키워드 : `if` `switch`
-                int result = calc.calculate(value1, value2, symbol);
+                int result = calculator.calculate(value1, value2, operator);
                 System.out.println("결과: " + result);
 
                 // 저장된 연산 결과 확인
-                System.out.println(calc.getResults());
+                System.out.println(calculator.getResults());
 
                 System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             } catch (Exception e) {
@@ -43,16 +43,14 @@ public class App {
             }
         } while (!sc.next().equals("exit"));
         // 수정 전 결과
-        System.out.println("수정 전 데이터:\n"+calc.getResults());
+        System.out.println("수정 전 데이터:\n"+calculator.getResults());
 
         // 연산 결과 수정
-        calc.setResults(0, 999);
-        System.out.println("0번째 인덱스를 999로 수정:\n"+calc.getResults());
+        calculator.setResult(0, 999);
+        System.out.println("0번째 인덱스를 999로 수정:\n"+calculator.getResults());
 
         // 가장 먼저 저장된 데이터 삭제
-        calc.removeResult();
-        System.out.println("가장 먼저 저장된 데이터 삭제: \n"+calc.getResults());
+        calculator.removeFirstResult();
+        System.out.println("가장 먼저 저장된 데이터 삭제: \n"+calculator.getResults());
     }
 }
-
-// 예외 질문 -> 오류 던질때? throw 쓸 때 예외 코드? Exeption or Illig~Argu~Exception? 어떻게 선택해야 하나요?
--- src/com/example/lv2/calculator/Calculator.java
+++ src/com/example/lv2/calculator/Calculator.java
@@ -1,34 +1,34 @@
 package com.example.lv2.calculator;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
     // 속성
-    private ArrayList results = new ArrayList();
+    private List<Integer> results = new ArrayList<>();
 
     // 생성자
 
-
     // 기능
-    public int calculate(int val1, int val2, char symbol) {
+    public int calculate(int value1, int value2, char operator) {
         // 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산 후 결과값 출력 _키워드 : `if` `switch`
         int result;
 
-        switch (symbol) {
+        switch (operator) {
             case '+':
-                result = val1 + val2;
+                result = value1 + value2;
                 break;
             case '-':
-                result = val1 - val2;
+                result = value1 - value2;
                 break;
             case '*':
-                result = val1 * val2;
+                result = value1 * value2;
                 break;
             case '/':
-                if (val2 == 0) {
+                if (value2 == 0) {
                     throw new RuntimeException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 } else {
-                    result = val1 / val2;
+                    result = value1 / value2;
                 }
                 break;
             default:
@@ -40,17 +40,17 @@ public class Calculator {
     }
 
     // 저장된 연산 결과 반환
-    public ArrayList getResults(){
+    public List<Integer> getResults(){
         return results;
     }
 
     // 연산 결과 수정
-    public void setResults(int idx, int value){
+    public void setResult(int idx, int value){
         results.set(idx, value);
     }
 
     // 가장 먼저 저장된 데이터 삭제
-    public void removeResult(){
+    public void removeFirstResult(){
         results.remove(0);
     }
 }
