0f5945cd890bacaf8f37b48096d46e0672cc34c5
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-30 11:55:09 +0900

    feat: 연산 기호에 맞는 사칙연산 수행 후 결과값 출력, 나눗셈 연산에서 분모에 0 입력 시 예외 처리

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -27,6 +27,30 @@ public class Calculator {
             System.out.print("사칙연산 기호를 입력하세요: ");
             input = scanner.nextLine();
             char operator = input.charAt(0);
+
+            // 사칙연산을 기호에 맞게 수행 후 result에 결과 저장
+            int result = 0;
+            switch (operator){
+                case '+':
+                    result = firstNum + lastNum;
+                    break;
+                case '-':
+                    result = firstNum - lastNum;
+                    break;
+                case '*':
+                    result = firstNum * lastNum;
+                    break;
+                case '/':
+                    if(lastNum == 0){
+                        throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    }else{
+                        result = firstNum / lastNum;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("결과: " + result);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
