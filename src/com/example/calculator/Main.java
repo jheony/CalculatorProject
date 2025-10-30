b68ba513b6d02a3c642cda6ce82c4f8fc6f5774e
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-30 13:12:30 +0900

    feat: 연산 결과 수정 기능 추가

--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input;
         int result;
-        int selectNum = 0;
+        int selectNum;
         Calculator calculator = new Calculator();
 
 
@@ -19,72 +19,77 @@ public class Main {
             System.out.println("1. 사칙연산\t\t2. 연산 결과 조회\t\t3. 연산 결과 수정\t\t4. 종료");
             try {
                 selectNum = inputNum();
-            } catch (Exception ignored) {
-            }
 
-            switch (selectNum) {
-                case 1:
-                    // 계산기 실행 중 체크
-                    boolean isCalculatorRunning = true;
-                    while (isCalculatorRunning) {
-                        try {
-                            // 0포함 양의 정수 2개 입력받기
-                            System.out.print("첫 번째 숫자를 입력하세요: ");
-                            int firstNum = inputNum();
+                switch (selectNum) {
+                    case 1:
+                        // 계산기 실행 중 체크
+                        boolean isCalculatorRunning = true;
+                        while (isCalculatorRunning) {
+                            try {
+                                // 0포함 양의 정수 2개 입력받기
+                                System.out.print("첫 번째 숫자를 입력하세요: ");
+                                int firstNum = inputNum();
 
-                            System.out.print("두 번째 숫자를 입력하세요: ");
-                            int lastNum = inputNum();
+                                System.out.print("두 번째 숫자를 입력하세요: ");
+                                int lastNum = inputNum();
 
-                            // 사칙연산 기호 입력받기
-                            System.out.print("사칙연산 기호를 입력하세요: ");
-                            input = scanner.nextLine();
-                            char operator = input.charAt(0);
+                                // 사칙연산 기호 입력받기
+                                System.out.print("사칙연산 기호를 입력하세요: ");
+                                input = scanner.nextLine();
+                                char operator = input.charAt(0);
 
-                            // 사칙연산 수행
-                            result = calculator.calculate(firstNum, lastNum, operator);
-                            System.out.println("결과: " + result);
+                                // 사칙연산 수행
+                                result = calculator.calculate(firstNum, lastNum, operator);
+                                System.out.println("결과: " + result);
 
-                            // 종료 여부 체크
-                            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                            input = scanner.nextLine();
-                            if (input.equals("exit")) {
-                                isCalculatorRunning = false;
-                                System.out.println("계산을 종료합니다.");
+                                // 종료 여부 체크
+                                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                                input = scanner.nextLine();
+                                if (input.equals("exit")) {
+                                    isCalculatorRunning = false;
+                                    System.out.println("계산을 종료합니다.");
+                                }
+                            } catch (IllegalArgumentException e) {
+                                System.out.println("0포함 양의 정수를 입력해주세요.\n");
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage() + "\n");
                             }
-                        } catch (IllegalArgumentException e) {
-                            System.out.println("0포함 양의 정수를 입력해주세요.\n");
-                        } catch (Exception e) {
-                            System.out.println(e.getMessage() + "\n");
                         }
-                    }
-                    break;
-                case 2:
-                    if(calculator.isResultsEmpty()){
-                        System.out.println("연산 결과가 없습니다.");
-                    }else{
-                        calculator.showResults();
-                    }
-                    break;
-                case 3:
-                    if(calculator.isResultsEmpty()){
-                        System.out.println("연산 결과가 없습니다.");
-                    }else{
-//                        System.out.print("수정할 인덱스를 입력해주세요.");
-//                        int idx = inputNum();
-//                        System.out.println("수정 결과값을 입력해주세요.");
-//                        int inputResult = inputNum();
-                    }
-                    break;
-                case 4:
-                    isRunning = false;
-                    System.out.println("계산기를 종료합니다.");
-                    break;
-                default:
-                    System.out.println("메뉴에 있는 번호를 선택해주세요.");
+                        break;
+                    case 2:
+                        if (calculator.isResultsEmpty()) {
+                            System.out.println("연산 결과가 없습니다.");
+                        } else {
+                            calculator.showResults();
+                        }
+                        break;
+                    case 3:
+                        if (calculator.isResultsEmpty()) {
+                            System.out.println("연산 결과가 없습니다.");
+                        } else {
+                            calculator.showResults();
+                            System.out.print("수정할 인덱스를 입력해주세요: ");
+                            int idx = inputNum();
+                            System.out.print("수정할 결과값을 입력해주세요: ");
+                            int inputResult = inputNum();
+                            calculator.setResult(idx, inputResult);
+
+                            System.out.println(idx + "번째의 결과값이 " + inputResult + "로 수정되었습니다.");
+                        }
+                        break;
+                    case 4:
+                        isRunning = false;
+                        System.out.println("계산기를 종료합니다.");
+                        break;
+                    default:
+                        throw new Exception("메뉴에 있는 번호를 선택해주세요.");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("0포함 양의 정수를 입력해주세요.\n");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
-
-
     }
 
     public static int inputNum() throws Exception {
