ba688da7b3a2a8677d658b43dc905a141ddf4966
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-30 12:17:57 +0900

    refactor: calculate 메서드에 입력값들을 매개변수로 전달

--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -6,14 +6,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
+        int result;
 
         Calculator calculator = new Calculator();
+
         // 계산기 실행 중 체크
         boolean isRunning = true;
         while (isRunning) {
             try {
-                // 입력 및 사칙연산 수행
-                int result = calculator.calculate();
+                // 0포함 양의 정수 2개 입력받기
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                input = scanner.nextLine();
+                int firstNum = Integer.parseInt(input);
+                if (firstNum < 0) {
+                    throw new Exception("0포함 양의 정수를 입력해주세요.");
+                }
+
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                input = scanner.nextLine();
+                int lastNum = Integer.parseInt(input);
+                if (lastNum < 0) {
+                    throw new Exception("0포함 양의 정수를 입력해주세요.");
+                }
+
+                // 사칙연산 기호 입력받기
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                input = scanner.nextLine();
+                char operator = input.charAt(0);
+
+                // 사칙연산 수행
+                result = calculator.calculate(firstNum, lastNum, operator);
                 System.out.println("결과: " + result);
 
                 // 종료 여부 체크
